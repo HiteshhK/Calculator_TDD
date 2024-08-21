bd039efab6dc95812c0c06406bfd9ae5daeba57a
Hitesh Kaushik <kaushikhitesh002@gmail.com>
2024-08-21 12:31:58 +0530

    adding split parameters

--- calculator.js
+++ calculator.js
@@ -2,12 +2,19 @@
 function add(numbers) {
     if (numbers === '')
         return 0;
-    var splitParameters = ","
+    var splitParameters = /[\n,]/;
+    if(numbers.charAt(0)==='/' && numbers.charAt(1)==='/'){
+        numbers = getNumbersFromString(numbers,splitParameters);
+    }
     var numberArr = numbers.split(splitParameters);
     return numberArr.reduce(function (acc, curr) {
         var number = parseInt(curr);
         return acc + number;
     }, 0);
+}
+
+function getNumbersFromString(numbers,splitParameters){
+
 }
 
 exports["default"] = add;
